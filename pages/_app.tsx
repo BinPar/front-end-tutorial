72d37a65797d35304c0c4e3dd1a3f41950469ba6
drnachio <nacho@binpar.com>
2022-02-01 11:04:28 +0100

    feat: styles in src

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../src/styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   // eslint-disable-next-line react/jsx-props-no-spreading
